342bbdb5d1c04bc209435099caf084235b65851e
Victor Petrov <victor.petrov@gmail.com>
2013-12-07 16:18:12 -0500

    Move www/survana/admin to www/admin

---
+++ www/admin/assets/js/controllers.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var adminControllers = angular.module('adminControllers', []);
+
+adminControllers.controller('HomeCtrl', ['$scope', '$http',
+    function HomeCtrl($scope, $http) {
+    }
+]);
+
+angular.module("admin").controller('LoginCtrl', ['$scope', '$http', '$location',
+    function LoginCtrl($scope, $http, $location) {
+        $scope.data = { key : "" };
+        $scope.doLogin = function (q) {
+            $http.post("login", $scope.data).success(function () {
+                console.log('SUCCESS, redirection to /');
+                $location.path("/");
+            }).error(function() {
+                    console.log('LOGIN FAILED');
+                })
+        }
+    }
+]);
