0490224b1f7ec33a6ace6cba784b3f21e2af8f6c
Victor Petrov <victor.petrov@gmail.com>
2014-05-10 20:34:23 -0400

    Move framework files into separate repo

--- src/neuroinformatics.harvard.edu/survana/auth/config.go
+++
@@ -1,8 +0,0 @@
-package auth
-
-type Config struct {
-    Type string `json:"type,omitempty"`
-    AllowRegistration bool `json:"allow_registration,omitempty"`
-    Username string `json:"username,omitempty"`
-    Password string `json:"password,omitempty"`
-}
