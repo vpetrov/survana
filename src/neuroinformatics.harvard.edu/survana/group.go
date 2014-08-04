c3be9ecd66f89dab423f5d7ca414535735e08dff
Victor Petrov <victor_petrov@harvard.edu>
2014-08-04 08:25:58 -0400

    Remove unused Group.NewGroup() and Group.FindGroup()

--- src/neuroinformatics.harvard.edu/survana/group.go
+++ src/neuroinformatics.harvard.edu/survana/group.go
@@ -9,25 +9,3 @@ type Group struct {
 	Id         *string `bson:"id,omitempty" json:"id,omitempty"`
 	Name       *string `bson:"name,omitempty" json:"name,omitempty"`
 }
-
-//creates a new group
-func NewGroup(name string) *Group {
-	return &Group{
-		Name: &name,
-	}
-}
-
-func FindGroup(id string, db orm.Database) (group *Group, err error) {
-	group = &Group{Id: &id}
-	err = db.Find(group)
-
-	if err != nil {
-		if err == orm.ErrNotFound {
-			err = nil
-		}
-
-		return nil, err
-	}
-
-	return
-}
