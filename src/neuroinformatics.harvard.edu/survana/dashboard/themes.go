fc121c6489c1e899a86c84839a63e2272f76af9e
Victor Petrov <victor_petrov@harvard.edu>
2014-08-25 15:49:32 -0400

    Removed unnecessary study publish template. Rename preview.html to form.html and preview-study.html to study.html

--- src/neuroinformatics.harvard.edu/survana/dashboard/themes.go
+++ src/neuroinformatics.harvard.edu/survana/dashboard/themes.go
@@ -6,13 +6,12 @@ import (
 )
 
 func (d *Dashboard) Theme(w http.ResponseWriter, r *perfect.Request) {
-	template_name := "index"
+	template_name := "form"
 
 	//get the form id
 	query := r.URL.Query()
 	theme_id := query.Get("id")
 	theme_version := query.Get("version")
-	theme_publish := query.Get("publish")
 	theme_study := query.Get("study")
 
 	//TODO: Validate alnum
@@ -21,14 +20,8 @@ func (d *Dashboard) Theme(w http.ResponseWriter, r *perfect.Request) {
 		return
 	}
 
-	if len(theme_publish) != 0 {
-		template_name = "publish"
-	} else {
-		template_name = "preview"
-	}
-
 	if len(theme_study) != 0 {
-		template_name += "-study"
+		template_name = "study"
 	}
 
 	d.Module.RenderTemplate(w, r, "theme/"+theme_id+"/"+theme_version+"/"+template_name, nil)
